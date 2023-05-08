f3d3bd37645a419af10b113d9945e3ca45e18ee4
Deva <deva.caralasingam@dishserve.com>
2023-05-08 09:54:28 +0530

    initial commit

---
+++ laravel-jwt/resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
+
+<p>Project 2 Dashboard</p>
+<a href="http://127.0.0.1:8000/dashboard">Project 1</a>
+{{--<label for="my-select">--}}
+{{--    Select Project--}}
+{{--</label><select name="my-select" id="my-select">--}}
+{{--    <option value="Project1">Project 1</option>--}}
+{{--    <option value="Project2">Project 2</option>--}}
+{{--</select>--}}
